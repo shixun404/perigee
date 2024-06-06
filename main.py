c0ba885f41db5736d351bb24c675463c196a9a00
shixun404 <shixun404@gmail.com>
2024-06-06 09:20:38 -0700

    upudate

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ import initnetwork
 import readfiles
 import writefiles
 from scipy.stats import  truncexpon
+
 test_num       = 1000                   # graph size
 len_of_neigh   = int(sys.argv[3])       # outbound neighbors
 len_of_test    = int(sys.argv[5])       # maximum neighbors may switch each round
@@ -26,7 +27,7 @@ DelayPercantage= 90                     # how do we score the performance of ind
 pathunlimit    = 2000                   # default delay between node during shortest path
 unlimit        = 9999                   # how do we value the unresponded nodes
 sys.setrecursionlimit(19999999)
-    
+
 [G,NodeDelay,NodeHash,LinkDelay,NeighborSets,IncomingLimit,OutNeighbor,IncomingNeighbor,bandwidth]=initnetwork.GenerateInitialNetwork(str(sys.argv[1]),sys.argv[6])
 # store specific round results
 set1=[0,4,8,16,32,64,128,256,384,512]
@@ -40,14 +41,21 @@ for r in range(RoundNum):
         writefiles.write(OutputDelayFile, G, NodeDelay, NodeDelay, OutNeighbor)
     # vanilla algorithm
     if str(sys.argv[2]) == 'vanilla':
-        new_neighbor=algvanilla.VanillaSelection(G, OutNeighbor, len_of_neigh, len_of_test, str(sys.argv[6]), LinkDelay, NodeDelay, IncomingLimit, NodeHash,  NeighborSets, r, sys.argv[4], IncomingNeighbor)
+        new_neighbor=algvanilla.VanillaSelection(G, OutNeighbor, len_of_neigh,
+                                                  len_of_test, str(sys.argv[6]),
+                                                    LinkDelay, NodeDelay, IncomingLimit,
+                                                      NodeHash,  NeighborSets,
+                                                        r, sys.argv[4], IncomingNeighbor)
     # ucb algorithm
     elif str(sys.argv[2]) == 'ucb':
         # initiat hist_score_tales, maintain the historical performance of all the connected nodes
         if r==0:
             hist_score_table=np.zeros([test_num,len_of_neigh,len_of_subround*RoundNum])
             hist_score_length=np.zeros([test_num,len_of_neigh])
-        [new_neighbor, hist_score_table, hist_score_length]= algucb.UCBelection(G, OutNeighbor, len_of_neigh, len_of_test, str(sys.argv[6]), LinkDelay, NodeDelay, IncomingLimit, NodeHash, hist_score_table, hist_score_length, NeighborSets, IncomingNeighbor)
+        [new_neighbor, hist_score_table, hist_score_length]= algucb.UCBelection(
+            G, OutNeighbor, len_of_neigh, len_of_test, str(sys.argv[6]), LinkDelay,
+              NodeDelay, IncomingLimit, NodeHash, hist_score_table, hist_score_length,
+                NeighborSets, IncomingNeighbor)
     # subset algorithm
     elif str(sys.argv[2]) == 'subset':
         new_neighbor= algsubset.SubsetSelection(G, OutNeighbor, len_of_neigh, len_of_test, str(sys.argv[6]), LinkDelay, NodeDelay, IncomingLimit, NodeHash, r, IncomingNeighbor)
